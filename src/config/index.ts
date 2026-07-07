f36fec7af07ae1f1bbcd234a96c6a27efd531a6b
ei-shadi <eftajulislam.saadi@gmail.com>
2026-07-07 23:52:03 +0600

    File Setup Completed

---
+++ src/config/index.ts
@@ -0,0 +1,24 @@
+import dotenv from "dotenv";
+import path from "path";
+import { env } from "process";
+
+
+dotenv.config({
+  quiet: true,
+  path: path.join(process.cwd(), ".env.local"),
+});
+
+
+const config = {
+  port: env.PORT || 3000,
+  environment: env.NODE_ENV || "development",
+  database_url: env.DATABASE_URL as string,
+  app_url : process.env.APP_URL,
+  bcrypt_salt_rounds : env.BCRYPT_SALT_ROUNDS,
+  jwt_access_secret : env.JWT_ACCESS_SECRET!,
+  jwt_refresh_secret : env.JWT_REFRESH_SECRET!,
+  jwt_access_expires_in : env.JWT_ACCESS_EXPIRES_IN!,
+  jwt_refresh_expires_in : env.JWT_REFRESH_EXPIRES_IN!
+};
+
+export default config;
