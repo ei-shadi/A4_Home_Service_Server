1ffb9dac5e3eb3c77eda9e89f4d424de8df773ac
ei-shadi <eftajulislam.saadi@gmail.com>
2026-07-08 01:07:00 +0600

    Prisma Schema Folder Setup Completed

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,20 @@
 import express, { Application, Request, Response } from "express";
-
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import config from "./config";
 
 
 const app : Application = express();
+
+
+// Middleware
+app.use(cors({
+    origin: config.app_url,
+    credentials: true,
+}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get("/", (req : Request, res : Response) => {
     res.send("Welcome To The Home Service!");
